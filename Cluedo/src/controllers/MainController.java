b5855d3b0cf4121e60b083d02479dd2c785edda9
Gio Cigaral <GioC@Super2Kids.co.nz>
2020-08-26 23:38:40 +1200

    Create base main window layout

---
+++ Cluedo/src/controllers/MainController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+public class MainController {
+
+    /*
+     * TODO: Store logic for stuff like action listeners here
+     *  - logic directly manipulating data in Models should be in relevant model class (e.g. handling coordinate movement of players)
+     * TODO: Store references to model here (use - import models.*)
+     * TODO: Update (or call update methods) of views here
+     */
+
+}
